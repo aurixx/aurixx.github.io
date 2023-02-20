e0fdb9e6938b985a0f60a40b04c94058e5897509
FLAK-ZOSO <83119697+FLAK-ZOSO@users.noreply.github.com>
2023-02-20 15:10:45 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
 function changeColor(color_) {
     document.getElementsByTagName('section')[0].style.backgroundColor = color_;
 }
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
 
 colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white', 'gray', 'cyan', 'magenta', 'lime'];
 function colorRotate() {
     while (true) {
         for (color of colors) {
-            setTimeout(changeColor(color), 100);
+            sleep(500).then(() => {
+                changeColor(color);
+            });
         }
     }
 };
